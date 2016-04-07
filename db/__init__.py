db8a7ea77b07587ddc5890727dbcbd374c6f416e
Robert Ross Wardrup <minorsecond@gmail.com>
2016-04-07 18:32:47 -0500

    Created metadata for postgresql table

--- db/__init__.py
+++ db/__init__.py
@@ -1,4 +1,26 @@
 """
-PostgreSQL database
+database files
 """
 
+from sqlalchemy import Column, Integer, String, Float, DateTime, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('postgresql://speedcam:Rward0232@localhost/speedcamdb')
+Base = declarative_base()
+
+__all__ = ['speeders']
+
+
+class Speeders(Base):
+    """
+    Table for Humans
+    """
+
+    __tablename__ = "speeders"
+    id = Column(Integer, primary_key=True, index=True)
+    uniqueID = Column(String, index=True)
+    datetime = Column(DateTime, index=True)
+    speed = Column(Float, index=True)
+    rating = Column(Integer)
+
+Base.metadata.create_all(engine, checkfirst=True)
