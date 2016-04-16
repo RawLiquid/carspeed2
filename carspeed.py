66df7f020eca097745c1ec919fa9d55a1794eb71
Robert Ross Wardrup <minorsecond@gmail.com>
2016-04-16 05:50:05 -0500

    Working on pixel grabber.

--- carspeed.py
+++ carspeed.py
@@ -178,6 +178,7 @@ def grab_rgb(image):
     points = np.where(mask == 255)
     for point in points:
         pixel = (image[points[1], points[0]])
+        pixel = pixel.tolist()
         pixels.append(pixel)
 
     print(pixels)
