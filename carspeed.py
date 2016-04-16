72b4b864cdf8fc113537eb417f0fd1edb7b6b705
Robert Ross Wardrup <minorsecond@gmail.com>
2016-04-16 10:46:25 -0500

    Add RGB value to database.

--- carspeed.py
+++ carspeed.py
@@ -189,7 +189,7 @@ def grab_rgb(image, c):
 
     points = np.where(mask == 255)
 
-    print(len(points))
+    print(len(c))
 
     for point in points:
         pixel = (image[point[1], point[0]])
