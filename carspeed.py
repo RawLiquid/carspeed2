0a0afe95189ee7a5a2d853dd13735a53059203e5
Robert Ross Wardrup <minorsecond@gmail.com>
2016-04-16 06:10:21 -0500

    Working on grab_rgb

--- carspeed.py
+++ carspeed.py
@@ -181,7 +181,7 @@ def grab_rgb(image):
     for point in points:
         pixel = (image[point[1], point[0]])
         pixel = pixel.tolist()
-        pixel = [set(tuple(x) for x in y) for y in pixel]
+        # pixel = [set(tuple(x) for x in y) for y in pixel]
         print(pixel)
         pixels.append(pixel)
 
