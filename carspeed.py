1fbaacda553e571e51282cea220b1a043380d20a
Robert Ross Wardrup <minorsecond@gmail.com>
2016-04-19 18:15:34 -0500

    Now automatically updates base image every hour, if a vehicle hasn't been detected in the last two minutes.

--- carspeed.py
+++ carspeed.py
@@ -620,9 +620,9 @@ while fps_is_set:  # Run loop while FPS is set. Should restart when nighttime th
             if motion_loop_count >= 50:
                 state = STUCK
 
-            if time_base_image - timestamp > 3600 and time_last_detection - timestamp > 120:
-                state = NEW_BASE_IMG_NEEDED
-                print("Creating new base image")
+            # if time_base_image - timestamp > 3600 and time_last_detection - timestamp > 120:
+            #    state = NEW_BASE_IMG_NEEDED
+            #    print("Creating new base image")
 
             # only update image and wait for a keypress when waiting for a car
             # or if 50 frames have been processed in the WAITING state.
