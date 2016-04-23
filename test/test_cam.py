4aefce5206ae987154e96108382b67a25cabb536
Robert Ross Wardrup <minorsecond@gmail.com>
2016-04-23 04:50:19 -0500

    Fixed unintended indent in test_cam code.

--- test/test_cam.py
+++ test/test_cam.py
@@ -12,9 +12,9 @@ def show_webcam(mirror=False):
         ret, img = cam.read()
         if mirror:
             img = cv2.flip(img, 1)
-            cv2.imshow('Camera Output', img)
-            if cv2.waitKey(1) == 27:
-                break  # esc to quit
+        cv2.imshow('Camera Output', img)
+        if cv2.waitKey(1) == 27:
+            break  # esc to quit
 
     cv2.destroyAllWindows()
 
