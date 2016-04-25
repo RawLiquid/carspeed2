2c23ba098a6a0f5912fae82a0a9046407fe1a48c
Robert Ross Wardrup <minorsecond@gmail.com>
2016-04-25 17:41:59 -0500

    clear image list after saving image.

--- carspeed.py
+++ carspeed.py
@@ -662,7 +662,8 @@ while fps_is_set:  # Run loop while FPS is set. Should restart when nighttime th
                                 time_last_detection = timestamp
                                 last_mph_detected = round(speed, 2)
                                 mph_list = []
-                                image_orig = image_list[len(image_list) - 2]
+
+                                image_orig = image_list[len(image_list) - 2]  # Get next-to-last image
                                 create_image(save_photos, SPEED_THRESHOLD, speed, image_orig, rectangle, image_width,
                                              image_height)
 
