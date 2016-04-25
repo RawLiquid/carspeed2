3baf8e553dc6fff9e56c5735d8bb39f2d1c699dc
Robert Ross Wardrup <minorsecond@gmail.com>
2016-04-25 17:22:10 -0500

    even smaller value for alpha (0.001)

--- test/test_cam.py
+++ test/test_cam.py
@@ -98,7 +98,7 @@ def test_processing(base, frame):
         if w * h >= MIN_AREA:
             rectangle = cv2.rectangle(clipped_orig, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    base = cv2.accumulateWeighted(gray, base, 0.01)  # attempt background removal
+    base = cv2.accumulateWeighted(gray, base, 0.001)  # attempt background removal
 
     return base, gray, thresh, rectangle
 
