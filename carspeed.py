0a448ef565780a91192ec0666091f1be7e306021
Robert Ross Wardrup <minorsecond@gmail.com>
2016-04-25 19:46:39 -0500

    Changed font size in output.

--- carspeed.py
+++ carspeed.py
@@ -404,8 +404,8 @@ def create_image(save_photos, speed_threshold, speed, image, rectangle, image_wi
 
         # timestamp the image
         cv2.putText(image, datetime.datetime.now().strftime("%d %B %I:%M:%S%p"),
-                    (10, image.shape[0] - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                    .3, (0, 255, 0), 2)
+                    (10, image.shape[0] - 3), cv2.FONT_HERSHEY_SIMPLEX,
+                    .3, (0, 255, 0), 1)
 
         # write the speed: first get the size of the text
         size, base = cv2.getTextSize("%.0f mph" % speed, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
@@ -414,8 +414,8 @@ def create_image(save_photos, speed_threshold, speed, image, rectangle, image_wi
         cntr_x = int((width - size[0]) / 2)
         y_location = int(height * 0.2)
         cv2.putText(image, "%.0f mph" % speed,
-                    (225, image.shape[0] - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                    .5, (0, 255, 0), 2)
+                    (250, image.shape[0] - 3), cv2.FONT_HERSHEY_SIMPLEX,
+                    .5, (0, 255, 0), 1)
 
         # and save the image to disk
         filename = "images/car_at_" + datetime.datetime.now().strftime(
