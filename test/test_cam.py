247c085c6757397db94e0aaa01108827746ed9c0
Robert Ross Wardrup <minorsecond@gmail.com>
2016-04-30 06:11:37 -0500

    Update test parameters

--- test/test_cam.py
+++ test/test_cam.py
@@ -124,9 +124,7 @@ def show_webcam(camera, capture):
             image = rotate_image(image)  # Rotate the image
             base_image, blurred, thresh, rectangle = test_processing(base_image, image)  # Run openCV image processing
 
-            gray = rotate_image(frame)
-            gray = gray.array
-            gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             gray = cv2.equalizeHist(gray)
 
             cv2.namedWindow('Base')
